89b1939d7015921298ede15df68b8a9d504898fc
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2021-07-27 19:28:07 +0530

    treesitter add comment parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ local ts = {}
 ts.config = function()
   local tsconfig = require('nvim-treesitter.configs')
   tsconfig.setup {
-    ensure_installed = { 'ruby', 'lua', 'bash', 'javascript', 'json', 'yaml', 'python', 'scss', 'html' },
+    ensure_installed = { 'ruby', 'lua', 'bash', 'javascript', 'json', 'yaml', 'python', 'scss', 'html', 'comment' },
     ignore_install = {},
     highlight = {
       enable = true
