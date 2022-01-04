b2da3336b7b8cbea579fc89a85457b33ddf24e84
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2022-01-04 14:41:04 +0530

    move options to its own file

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 ---@diagnostic disable: undefined-global
 -- install packer.nvim retroactively
+
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
@@ -10,48 +11,4 @@ if fn.empty(fn.glob(install_path)) > 0 then
   execute 'packadd packer.nvim'
 end
 
---Make line numbers default
-vim.wo.number = true
-vim.wo.relativenumber = true
-
---Enable mouse mode
-vim.o.mouse = 'a'
-
---Enable break indent
-vim.o.breakindent = true
-
---Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
---Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
-
---Set colorscheme (order is important here)
-vim.o.termguicolors = true
-
--- Clipboard
-if vim.fn.has('unix') then
-  vim.o.clipboard = 'unnamedplus'
-else
-  vim.o.clipboard = 'unnamed'
-end
-
--- Completion
-vim.opt.shortmess:append('c')
-vim.opt.completeopt= { 'menu', 'menuone', 'noselect' }
-
--- Cursorline
-vim.o.cursorline = true
-
--- Identation
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.tabstop = 4
-vim.o.textwidth = 120
-vim.o.modeline = true
-
--- ColorColumn
-vim.opt.colorcolumn:append('+1')
+require('options')
---
+++ lua/options.lua
@@ -0,0 +1,47 @@
+---@diagnostic disable: undefined-global
+
+--Make line numbers default
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+--Enable mouse mode
+vim.o.mouse = 'a'
+
+--Enable break indent
+vim.o.breakindent = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+--Set colorscheme (order is important here)
+vim.o.termguicolors = true
+
+-- Clipboard
+if vim.fn.has('unix') then
+  vim.o.clipboard = 'unnamedplus'
+else
+  vim.o.clipboard = 'unnamed'
+end
+
+-- Completion
+vim.opt.shortmess:append('c')
+vim.opt.completeopt= { 'menu', 'menuone', 'noselect' }
+
+-- Cursorline
+vim.o.cursorline = true
+
+-- Identation
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 4
+vim.o.textwidth = 120
+vim.o.modeline = true
+
+-- ColorColumn
+vim.opt.colorcolumn:append('+1')
