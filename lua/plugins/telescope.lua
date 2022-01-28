18a909471bd13b2b631a65c55cbd80d18575ab94
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2022-01-28 16:55:38 +0530

    telescope file picker ascending sorting_strategy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ local t = {}
 t.config = function()
   require('telescope').setup {
     defaults = {
+      sorting_strategy = "ascending",
       layout_config = {
          horizontal = {
             prompt_position = 'top'
