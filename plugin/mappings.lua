5b983f57e7738c2664d099568d67fdc92dfd4fb4
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2022-03-07 13:40:47 +0530

    add mappings to aerial

--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -44,4 +44,16 @@ map('n', '<leader>dd', [[<cmd>TroubleToggle document_diagnostics<CR>]], opt)
 map('n', '<leader>s', [[<Cmd>set spell!<CR>]], opt)
 map('n', '<leader>fs', [[1z=]], opt)
 map('n', '<Esc>', [[<Cmd>:nohlsearch<CR>]], opt)
-map('n', '<F10>', [[<Cmd>AerialToggle!<CR>]], opt)
+
+require("aerial").setup({
+  on_attach = function(bufnr)
+    -- Toggle the aerial window with <leader>a
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<F10>', '<cmd>AerialToggle!<CR>', {})
+    -- Jump forwards/backwards with '{' and '}'
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '{', '<cmd>AerialPrev<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '}', '<cmd>AerialNext<CR>', {})
+    -- Jump up the tree with '[[' or ']]'
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[[', '<cmd>AerialPrevUp<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']]', '<cmd>AerialNextUp<CR>', {})
+  end
+})
