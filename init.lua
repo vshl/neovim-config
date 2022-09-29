46f9c66ff7319ef4c7fb7ce309fcdb6a908c6f64
Vishal Ravishankar <rsh.vishal@gmail.com>
2022-09-29 18:24:40 +0530

    add: author name

--- init.lua
+++ init.lua
@@ -1,14 +1,1 @@
--- install packer.nvim retroactively
-
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
-end
-
-require('options')
-require('commands')
+-- author: Vishal Ravishankar
