c5257f1e5efb3f3caa763a3d3ffde92362d71ccd
Vishal Ravishankar <rsh.vishal@gmail.com>
2022-10-12 11:28:04 +0530

    cleanup mapppings; make them uniform

--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -11,40 +11,40 @@ local opt = {}
 
 -- Telescope
 
-map('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]], opt)
-map('n', '<leader>lg', [[<cmd>Telescope live_grep<CR>]], opt)
-map('n', '<leader>gs', [[<cmd>Telescope grep_string<CR>]], opt)
-map('n', '<leader>bf', [[<cmd>Telescope buffers<CR>]], opt)
-map('n', '<leader>fb', [[<cmd>Telescope file_browser<CR>]], opt)
-map('n', '<leader>fp', [[<cmd>lua require 'telescope'.extensions.file_browser.file_browser({path = vim.fn.expand('%:p:h')})<CR>]], opt)
-map('n', '<leader>fh', [[<cmd>Telescope help_tags<CR>]], opt)
-map('n', '<leader>ds', [[<cmd>Telescope lsp_document_symbols<CR>]], opt)
-map('n', '<leader>ws', [[<cmd>Telescope lsp_workspace_symbols<CR>]], opt)
-map('n', '<leader>ld', [[<cmd>Telescope diagnostics buffnr=0<CR>]], opt)
-map('n', '<leader>lw', [[<cmd>Telescope diagnostics<CR>]], opt)
-map('n', '<leader>cm', [[<cmd>Telescope commands<CR>]], opt)
-map('n', '<leader>km', [[<cmd>Telescope keymaps<CR>]], opt)
-map('n', '<leader>gc', [[<cmd>Telescope git_commits<CR>]], opt)
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>', opt)
+map('n', '<leader>lg', '<CMD>Telescope live_grep<CR>', opt)
+map('n', '<leader>gs', '<CMD>Telescope grep_string<CR>', opt)
+map('n', '<leader>bf', '<CMD>Telescope buffers<CR>', opt)
+map('n', '<leader>fb', '<CMD>Telescope file_browser<CR>', opt)
+map('n', '<leader>fp', "<CMD>lua require 'telescope'.extensions.file_browser.file_browser({path = vim.fn.expand('%:p:h')})<CR>", opt)
+map('n', '<leader>fh', '<CMD>Telescope help_tags<CR>', opt)
+map('n', '<leader>ds', '<CMD>Telescope lsp_document_symbols<CR>', opt)
+map('n', '<leader>ws', '<CMD>Telescope lsp_workspace_symbols<CR>', opt)
+map('n', '<leader>ld', '<CMD>Telescope diagnostics buffnr=0<CR>', opt)
+map('n', '<leader>lw', '<CMD>Telescope diagnostics<CR>', opt)
+map('n', '<leader>cm', '<CMD>Telescope commands<CR>', opt)
+map('n', '<leader>km', '<CMD>Telescope keymaps<CR>', opt)
+map('n', '<leader>gc', '<CMD>Telescope git_commits<CR>', opt)
 
 -- ZenMode
-map('n', '<leader>z', [[<Cmd> ZenMode<CR>]], opt)
+map('n', '<leader>z', '<CMD>ZenMode<CR>', opt)
 
 -- FTerm.nvim
 map('n', '<A-i>', '<CMD>FTermToggle<CR>')
 map('t', '<A-i>', '<C-\\><C-n><CMD>FTermToggle<CR>')
 
 -- Misc
-map('n', '<leader>s', [[<Cmd>set spell!<CR>]], opt)
+map('n', '<leader>s', '<CMD>set spell!<CR>', opt)
 map('n', '<leader>fs', [[1z=]], opt)
-map('n', '<space>c', ':nohlsearch<CR>', opt)
+map('n', '<space>c', '<CMD>nohlsearch<CR>', opt)
 
 -- Diffview
 map('n', '<leader>do', '<CMD>DiffviewOpen<CR>', opt)
 map('n', '<leader>dc', '<CMD>DiffviewClose<CR>', opt)
 
 -- focus.nvim
-map('n', '<c-l>', ':FocusSplitNicely<CR>', opt)
-map('n', '<A-f>', ':FocusToggle<CR>', opt)
+map('n', '<c-l>', '<CMD>FocusSplitNicely<CR>', opt)
+map('n', '<A-f>', '<CMD>FocusToggle<CR>', opt)
 local focusmap = function(direction)
     map('n', "<A-" ..direction..">", ":lua require'focus'.split_command('"..direction.."')<CR>", opt)
 end
