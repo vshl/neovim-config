45507dc28cf0b955b554e0386520b8702a0a54a0
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-01-03 18:11:13 +0530

    change: dim inactive in colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
 return {
         'folke/tokyonight.nvim',
         config = function()
+                require('tokyonight').setup({
+                        dim_inactive = true
+                })
                 vim.cmd('colorscheme tokyonight')
         end
 }
