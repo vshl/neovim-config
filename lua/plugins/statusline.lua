1115d2e86e855380ad597d60d0e0760af0b277f9
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-02-06 12:50:23 +0530

    update: statusline; remove winbar

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,107 +1,95 @@
 local M = {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'kyazdani42/nvim-web-devicons',
-    'nord.nvim'
-  }
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+                'kyazdani42/nvim-web-devicons',
+                'nord.nvim'
+        }
 }
 
 M.priority = 100
 
 M.config = function()
-  local navic = require('nvim-navic')
-  -- Lsp server name .
-  local function server_name()
-    local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
-    return msg
-  end
+        local navic = require('nvim-navic')
+        -- Lsp server name .
+        local function server_name()
+                local msg = 'No Active Lsp'
+                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                local clients = vim.lsp.get_active_clients()
+                if next(clients) == nil then
+                        return msg
+                end
+                for _, client in ipairs(clients) do
+                        local filetypes = client.config.filetypes
+                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                                return client.name
+                        end
+                end
+                return msg
+        end
 
-  require('lualine').setup {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = '|',
-      section_separators = { left = '', right = '' },
-      disabled_filetypes = {}
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = {},
-      lualine_c = { 'buffers' },
-      lualine_x = { 'encoding', 'fileformat' },
-      lualine_y = {},
-      lualine_z = { 'branch' }
-    },
-    inactive_sections = {
-      lualine_c = {}
-    },
-    winbar = {
-      lualine_a = {
-        {
-          'filename',
-          path = 1,
-          symbols = {
-            modified = '●',
-            readonly = ''
-          },
+        require('lualine').setup {
+                options = {
+                        icons_enabled = true,
+                        theme = 'auto',
+                        component_separators = '|',
+                        section_separators = { left = '', right = '' },
+                        disabled_filetypes = {}
+                },
+                sections = {
+                        lualine_a = { 'mode' },
+                        lualine_b = {
+                                {
+                                        'filename',
+                                        path = 1,
+                                        symbols = {
+                                                modified = '●',
+                                                readonly = ''
+                                        },
+                                }
+                        },
+                        lualine_c = {
+                                {
+                                        'filetype',
+                                        icon_only = true
+                                },
+                                {
+                                        'diagnostics',
+                                        sources = { 'nvim_lsp' },
+                                        symbols = { error = ' ', warn = ' ', info = ' ' },
+                                        on_click = function()
+                                                vim.cmd('Telescope diagnostics buffnr=0')
+                                        end
+                                }
+                        },
+                        lualine_x = {
+                                {
+                                        'diff',
+                                        symbols = { added = ' ', modified = ' ', removed = ' ' }
+                                },
+                                {
+                                        server_name,
+                                        icon = { ' ', color = { gui = 'bold' } }
+                                }
+                        },
+                        lualine_y = { 'encoding', 'fileformat' },
+                        lualine_z = { 'location', 'progress', 'branch' }
+                },
+                inactive_sections = {
+                        lualine_a = {},
+                        lualine_b = {
+                                {
+                                        'filename',
+                                        path = 1,
+                                        symbols = {
+                                                modified = '●',
+                                                readonly = ''
+                                        }
+                                }
+                        },
+                        lualine_c = {}
+                },
+                extensions = { 'quickfix' }
         }
-      },
-      lualine_b = {
-        {
-          'filetype',
-          icon_only = true
-        },
-        {
-          'diagnostics',
-          sources = { 'nvim_lsp' },
-          symbols = { error = ' ', warn = ' ', info = ' ' },
-          on_click = function()
-            vim.cmd('Telescope diagnostics buffnr=0')
-          end
-        }
-      },
-      lualine_c = {
-        { navic.get_location, cond = navic.is_available }
-      },
-      lualine_x = {
-        {
-          server_name,
-          icon = { ' ', color = { gui = 'bold' } }
-        },
-        {
-          'diff',
-          symbols = { added = ' ', modified = ' ', removed = ' ' }
-        }
-      },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' }
-    },
-    inactive_winbar = {
-      lualine_c = {
-        {
-          'filename',
-          path = 1,
-          symbols = {
-            modified = '●',
-            readonly = ''
-          },
-        }
-      },
-      lualine_x = { 'location' }
-    },
-    extensions = { 'quickfix' }
-  }
 end
 
 return M
