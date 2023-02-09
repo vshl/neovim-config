95324231ecc2fbddfec079060e553a2db40edc79
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-02-09 12:03:05 +0530

    change: colorscheme options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
         'shaunsingh/nord.nvim',
         config = function()
+                vim.g.nord_contrast = true
+                vim.g.nord_borders = true
                 require('nord').set()
         end
 }
