2976f4eaa120e10747bfbcddf698cc55eb14ee21
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-05-10 15:21:37 +0530

    add: wsl clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -26,6 +26,21 @@ else
   vim.o.clipboard = 'unnamed'
 end
 
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
 -- Completion
 vim.opt.shortmess:append('c')
 vim.opt.completeopt= { 'menu', 'menuone', 'noselect' }
