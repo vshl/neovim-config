9a111aa64721120c1891f2eb234e65e62875af66
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-06-08 17:56:54 +0530

    change: neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,11 @@ return {
         },
         config = function()
                 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+                require('neo-tree').setup({
+                        filesystem = {
+                                follow_current_file = true
+                        }
+                })
         end,
         cmd = 'Neotree'
 }
