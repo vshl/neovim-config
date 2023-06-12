550dd6b08aab014c5be241c71fdd210664bbf1de
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-06-12 13:43:44 +0530

    change: lsp-kind nerd-fonts v3 support in nvim-cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -62,9 +62,17 @@ function M.config()
                         { name = 'nvim_lua' }
                 },
                 formatting = {
-                        format = function(_entry, vim_item)
-                                vim_item.kind = kind.presets.default[vim_item.kind]
-                                return vim_item
+                        format = function(entry, vim_item)
+                                if vim.tbl_contains({ 'path' }, entry.source.name) then
+                                        local icon, hl_group = require('nvim-web-devicons').get_icon(entry
+                                        :get_completion_item().label)
+                                        if icon then
+                                                vim_item.kind = icon
+                                                vim_item.kind_hl_group = hl_group
+                                                return vim_item
+                                        end
+                                end
+                                return require('lspkind').cmp_format({ with_text = false })(entry, vim_item)
                         end
                 }
         }
