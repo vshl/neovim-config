21b43c99e778f043ded42a238f257025fcf94569
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-06-27 21:06:40 +0530

    install: nvim-cmp/cmdline

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,6 +6,7 @@ local M = {
                 'hrsh7th/cmp-buffer',
                 'hrsh7th/cmp-emoji',
                 'hrsh7th/cmp-path',
+                'hrsh7th/cmp-cmdline',
                 'saadparwaiz1/cmp_luasnip',
                 'onsails/lspkind.nvim',
                 { 'hrsh7th/cmp-nvim-lua', ft = 'lua' }
@@ -76,6 +77,23 @@ function M.config()
                         end
                 }
         }
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                        { name = 'buffer' }
+                }
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                        { name = 'path' }
+                }, {
+                                { name = 'cmdline' }
+                        })
+        })
 end
 
 return M
