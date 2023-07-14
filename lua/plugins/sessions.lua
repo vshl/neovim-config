b0017cfcfdf749f8fab3bb8ac3d0625531717aa0
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-07-14 14:43:58 +0530

    install: nvim-possession
    remove auto-session for nvim-possession

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,25 @@
+return {
+    "gennaro-tedesco/nvim-possession",
+    dependencies = {
+        "ibhagwan/fzf-lua",
+    },
+    config = true,
+    init = function()
+        local possession = require("nvim-possession")
+        vim.keymap.set("n", "<leader>sl", function()
+            possession.list()
+        end)
+        vim.keymap.set("n", "<leader>sn", function()
+            possession.new()
+        end)
+        vim.keymap.set("n", "<leader>su", function()
+            possession.update()
+        end)
+        vim.keymap.set("n", "<leader>sd", function()
+            possession.delete()
+        end)
+        require("nvim-possession").setup({
+            autoload = true -- default false
+        })
+    end,
+}
