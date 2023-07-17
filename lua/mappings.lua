86bd255a5ea12b67e76456b5b50df8fb70c6f2ac
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-07-17 13:13:22 +0530

    update: neo-tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -77,7 +77,7 @@ map('n', '<leader>rp', '<Plug>RestNvim', opt)
 map('n', '<leader>rl', '<Plug>RestNvimLast', opt)
 
 -- neo-tree
-map('n', '<leader>\\', '<CMD>Neotree reveal<CR>', opt)
+map('n', '<leader>\\', '<CMD>Neotree toggle<CR>', opt)
 
 -- terminal.nvim
 local term_map = require("terminal.mappings")
