adcfd048edde963d4f0e9d779d1b59a97c4fabe8
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-08-01 15:57:15 +0530

    change: fzf config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,15 +1,22 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({
-      winopts_fn = function()
-        -- smaller width if neovim win has over 80 columns
-        return { width = vim.o.columns > 80 and 0.65 or 0.85 }
-      end,
-    })
-  end,
-  cmd = 'FzfLua'
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        -- calling `setup` is optional for customization
+        require("fzf-lua").setup({
+            winopts_fn = function()
+                -- smaller width if neovim win has over 80 columns
+                return { width = vim.o.columns > 80 and 0.65 or 0.85 }
+            end,
+
+            winopts = {
+                preview = {
+                    default = 'bat',
+                    layout = 'flex'
+                }
+            }
+        })
+    end,
+    cmd = 'FzfLua'
 }
