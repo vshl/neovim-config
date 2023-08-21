7d8089310d7707e9c8f0c2296a47dce6bc30ca6f
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-08-21 14:05:15 +0530

    install: hardtime.nvim

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+   "m4xshen/hardtime.nvim",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {
+        -- Add "oil" to the disabled_filetypes
+        disabled_filetypes = { "qf", "netrw", "neo-tree", "lazy", "mason", "oil" },
+    }
+}
