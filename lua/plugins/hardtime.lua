c8838a23c3dbca5e774b1b4e0297fea15253e65d
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-08-24 12:25:38 +0530

    change: hardtime disabled filetypes

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,6 +2,16 @@ return {
    "m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
    opts = {
-        disabled_filetypes = { "qf", "netrw", "neo-tree", "lazy", "mason", "NeogitStatus", "NeogitPopup" },
+        disabled_filetypes = {
+            "qf",
+            "netrw",
+            "neo-tree",
+            "lazy",
+            "mason",
+            "NeogitStatus",
+            "NeogitPopup",
+            "NeogitCommitMessage",
+            "fzf"
+        },
     }
 }
