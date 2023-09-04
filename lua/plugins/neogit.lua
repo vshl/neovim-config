2b26ca5b2de93b39831314f97b7857fc4e4a8cd7
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-09-04 17:25:24 +0530

    change: neogit config

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,9 +1,19 @@
 return {
     'NeogitOrg/neogit',
-    dependencies = 'nvim-lua/plenary.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'ibhagwan/fzf-lua',
+        'sindrets/diffview.nvim'
+    },
     cmd = { 'Neogit' },
     config = true,
     init = function()
-        require('neogit').setup {}
+        require('neogit').setup {
+            integrations = {
+                telescope = false,
+                diffview = true,
+                fzf_lua = true
+            }
+        }
     end
 }
