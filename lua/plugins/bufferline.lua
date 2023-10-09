119ce5140bb10fd72e3f1981e3450e54b86aa3f4
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-10-09 12:50:54 +0530

    remove: bufferline

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-        'akinsho/bufferline.nvim',
-        version = 'v3.*',
-        dependencies = 'nvim-web-devicons',
-        config = function()
-                require('bufferline').setup()
-        end,
-        event = 'BufRead'
-}
