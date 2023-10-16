3595b75ccb0b8f195f2505b36b6b58cab9809810
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-10-16 17:14:53 +0530

    change: rainbow delimiters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -98,7 +98,7 @@ M.config = function()
     }
 
     local rainbow_delimiters = require('rainbow-delimiters')
-    require('rainbow-delimiters.setup') {
+    require('rainbow-delimiters.setup').setup {
         strategy = {
             [''] = rainbow_delimiters.strategy['global'],
             vim = rainbow_delimiters.strategy['local'],
