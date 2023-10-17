84d6c3d8638bdced3e90376ed78d6d92dc01ec81
Vishal Ravishankar <rsh.vishal@gmail.com>
2023-10-17 16:17:22 +0530

    update: dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -4,17 +4,20 @@ return {
     config = function()
         local db = require('dashboard')
         db.setup({
-            theme = 'hyper',
             config = {
                 project = { action = 'FzfLua files cwd='},
                 week_header = {
                     enable = true,
                 },
                 shortcut = {
-                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                    {
+                        desc = '󰊳 Update',
+                        group = '@property',
+                        action = 'Lazy update',
+                        key = 'u'
+                    },
                     {
                         icon = ' ',
-                        icon_hl = '@variable',
                         desc = 'Files',
                         group = 'Label',
                         action = 'FzfLua files',
@@ -23,6 +26,7 @@ return {
                     {
                         icon = ' ',
                         desc = 'Sessions',
+                        group = '@variable',
                         action = "lua require('nvim-possession').list()",
                         key = 's'
                     }
