7e68a0caeff576396879c8582c4443debdf3a407
Vishal Ravishankar <rsh.vishal@gmail.com>
2024-02-14 15:09:05 +0530

    Revert "ignore terminals during session save"
    This reverts commit 405b326b65b0d3d006278f4fb98feb4753f56468.

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -21,19 +21,7 @@ return {
         require("nvim-possession").setup({
             save_hook = function()
                 vim.cmd("Neotree close")
-                -- Get visible buffers
-                local visible_buffers = {}
-                for _, win in ipairs(vim.api.nvim_list_wins()) do
-                    visible_buffers[vim.api.nvim_win_get_buf(win)] = true
-                end
-
-                local buflist = vim.api.nvim_list_bufs()
-                for _, bufnr in ipairs(buflist) do
-                    if visible_buffers[bufnr] == nil then -- Delete buffer if not visible
-                        vim.cmd("bd " .. bufnr)
-                    end
-                end
-            end
+            end,
         })
     end,
 }
