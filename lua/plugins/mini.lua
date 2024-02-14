4166e9a1ec748773fb68c88f9065ee8339372edc
Vishal Ravishankar <rsh.vishal@gmail.com>
2024-02-14 12:44:35 +0530

    install: mini.animate

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'echasnovski/mini.align',
+        version = false,
+        config  = true,
+        event   = 'VeryLazy'
+    },
+
+    {
+        'echasnovski/mini.animate',
+        version = false,
+        config = true,
+        event = 'VeryLazy'
+    }
+}
