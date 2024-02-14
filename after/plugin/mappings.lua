19b7c4e316b3c3c2c3c7399d4d6a906f99f5a1d7
Vishal Ravishankar <rsh.vishal@gmail.com>
2024-02-14 14:16:47 +0530

    install: toggleterm

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -80,9 +80,45 @@ map('n', '<leader>rl', '<Plug>RestNvimLast', opt)
 -- neo-tree
 map('n', '<leader>\\', '<CMD>Neotree toggle<CR>', opt)
 
--- neogit
-map('n', '<leader>gg', '<CMD>Neogit<CR>')
-map('n', '<leader>gc', '<CMD>Neogit commit<CR>')
+-- nvim-toggleterm
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  map('t', '<esc>', [[<C-\><C-n>]], opts)
+  map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  map('t', '<C-w>', [[<C-\><C-n><C-w><C-c>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+
+-- toggleterm lazygit
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+map('n', '<A-g>', '<cmd>lua _lazygit_toggle()<CR>')
 
 -- harpoon
 local harpoon = require('harpoon')
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,10 @@
+return {
+    'akinsho/nvim-toggleterm.lua',
+    version = '*',
+    config = true,
+    cmd = { 'ToggleTerm' },
+    keys = {
+        { '<A-i>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',      mode = { 'n' } },
+        { '<A-i>', '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', mode = { 'i' } },
+    }
+}
