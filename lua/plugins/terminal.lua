19cfb85add77105cd1c02167bf713d57d57f1a8c
Vishal Ravishankar <rsh.vishal@gmail.com>
2024-02-22 15:36:07 +0530

    change: keymap

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -5,6 +5,6 @@ return {
     cmd = { 'ToggleTerm' },
     keys = {
         { '<A-i>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',      mode = { 'n' } },
-        { '<A-i>', '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', mode = { 'i' } },
+        { '<A-i>', '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', mode = { 'i', 't' } },
     }
 }
