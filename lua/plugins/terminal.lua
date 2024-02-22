c66bc12b3a7907cc22efd7422ee3c85c56bace66
Vishal Ravishankar <rsh.vishal@gmail.com>
2024-02-22 15:40:30 +0530

    change: lazy event

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,7 +2,7 @@ return {
     'akinsho/nvim-toggleterm.lua',
     version = '*',
     config = true,
-    cmd = { 'ToggleTerm' },
+    event = 'VeryLazy',
     keys = {
         { '<A-i>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>',      mode = { 'n' } },
         { '<A-i>', '<Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', mode = { 'i', 't' } },
